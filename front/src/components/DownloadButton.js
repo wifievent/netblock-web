b25099c75bb51dd674c09fae00ac205210e2880d
Two-Weeks <webmon@kakao.com>
2021-10-13 20:29:07 +0900

    🌱 implement: route button

--- front/src/components/DownloadButton.js
+++ front/src/components/DownloadButton.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 const DownloadButton = (props) => {
+  const redirect = () => {
+    if (props.name === "windows") {
+      window.location.href = "/api/netblock/?os=window";
+    } else if (props.name === "linux") {
+      window.location.href = "/api/netblock?os=linux";
+    }
+  };
   return (
     <>
       <div className="dBtnDiv">
-        <Button
-          className="mt-1"
-          variant="dark"
-          size="lg"
-          onClick={() => (window.location.href = "/api/netblock")}
-        >
+        <Button className="mt-1" variant="dark" size="lg" onClick={redirect}>
           Download
         </Button>
         <h5 className="btnDesc">{props.desc}</h5>
