f6122f0523e7630373ff2f8d885c4b758c84a636
hackloud <rladydrlaqkq@gmail.com>
2021-10-31 23:46:28 +0900

    🥅 Fix: error handler
    remove render logic

--- back/loaders/express.js
+++ back/loaders/express.js
@@ -60,13 +60,11 @@ module.exports =  async ({ app }) => {
   
   //error handling
   app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+    res.status(err.status || 500).json(err.message);
   });
 
   const port = normalizePort(process.env.PORT || '3000');
