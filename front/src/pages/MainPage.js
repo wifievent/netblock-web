b00e9c887627242f42f5cd9413e0201ef087cdfa
Two-Weeks <webmon@kakao.com>
2021-11-01 19:58:57 +0900

    🔨 fix: main page

--- front/src/pages/MainPage.js
+++ front/src/pages/MainPage.js
@@ -1,15 +1,10 @@
 import React from "react";
 import Contents from "../components/Contents";
-import MainDownloadButton from "../components/MainDownloadButton";
-import { Link } from "react-router-dom";
 
 const MainPage = () => {
   return (
     <div className="mainPage">
       <Contents></Contents>
-      <Link to="/download">
-        <MainDownloadButton />
-      </Link>
     </div>
   );
 };
