4fca1a5df9428f434af6d76a374a2cd38b61a84b
Two-Weeks <webmon@kakao.com>
2021-11-03 11:15:38 +0900

    🔨 refactor: table content

--- front/src/components/CompTable.js
+++ front/src/components/CompTable.js
@@ -12,7 +12,7 @@ const CompTable = () => {
         <animated.div style={props}>
           <Row className="tableRow">
             <Col md="auto">
-              <div className="aboutDesc" style={{ fontSize: "25px" }}>
+              <div className="aboutDesc">
                 왜 <span style={{ fontWeight: "700" }}>NetBlock</span> 인가요 ?
               </div>
               <Table borderless="true" className="compTable">
@@ -26,28 +26,28 @@ const CompTable = () => {
                 </thead>
                 <tbody>
                   <tr>
-                    <td>추가 설치</td>
+                    <td>하나의 프로그램</td>
                     <td>●</td>
+                    <td>X</td>
+                    <td>X</td>
+                  </tr>
+                  <tr>
+                    <td>조작 불필요</td>
+                    <td>●</td>
+                    <td>X</td>
+                    <td>X</td>
+                  </tr>
+                  <tr>
+                    <td>추가 계정 불필요</td>
+                    <td>●</td>
+                    <td>X</td>
                     <td>O</td>
-                    <td>X</td>
-                  </tr>
-                  <tr>
-                    <td>직접 조작</td>
-                    <td>●</td>
-                    <td>X</td>
-                    <td>X</td>
-                  </tr>
-                  <tr>
-                    <td>자녀 계정</td>
-                    <td>●</td>
-                    <td>X</td>
-                    <td>X</td>
                   </tr>
                   <tr className="tabColBottom">
-                    <td>추가 인증</td>
+                    <td>인증 절차 제거</td>
                     <td className="tableColBottom">●</td>
                     <td>X</td>
-                    <td>X</td>
+                    <td>O</td>
                   </tr>
                 </tbody>
               </Table>
