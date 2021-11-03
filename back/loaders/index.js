12b8419423561acca5c3feab6c9a58a3ac9228c9
sophia5460 <sopahia4460@gmail.com>
2021-11-03 15:34:38 +0900

    change app.js to loader

--- back/loaders/index.js
+++ back/loaders/index.js
@@ -1,9 +1,11 @@
 const expressLoader = require('./express');
 const sequelizeLoader = require('./sequelize');
 const swaggerLoader = require('./swagger');
+const passportLoader = require('./passport');
 
 const init = async ({ expressApp }) => {
   await sequelizeLoader();
+  await passportLoader({ app: expressApp });
   await expressLoader({ app: expressApp });
   await swaggerLoader({ app: expressApp });
 };
