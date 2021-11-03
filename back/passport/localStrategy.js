4bed15ee82e3b7d3dc91c859c85e7ec14a574c51
sophia5460 <sopahia4460@gmail.com>
2021-11-03 13:26:47 +0900

    change to crypto

--- back/passport/localStrategy.js
+++ back/passport/localStrategy.js
@@ -1,22 +1,17 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const { User } = require('../models');
 module.exports = () => {
     passport.use(new LocalStrategy({
         usernameField: 'uid',
         passwordField: 'pw',
     }, async (uid, pw, done) => {
-        const exUser = await User.findOne({ where: { uid } }).catch((err) => console.error(err));
+        const exUser = await User.findOne({ where: { uid, pw: crypto.createHash('sha512').update(pw).digest('base64') } }).catch((err) => console.error(err));
         if (exUser) {
-            const result = await bcrypt.compare(pw, exUser.pw).catch((err) => console.error(err));
-            if (result) {
-                done(null, exUser);
-            } else {
-                done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
-            }
+            return done(null, exUser);
         } else {
-            done(null, false, { message: '가입되지 않은 회원입니다.' });
+            done(null, false, { message: '아이디 또는 비밀번호를 확인하세요.' });
         }
     }));
 };
