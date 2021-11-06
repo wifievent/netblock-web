14160c0f1a30e339588deb63e4554610dbc38aeb
Two-Weeks <webmon@kakao.com>
2021-11-06 21:35:10 +0900

    🔨 refactor: page route

--- front/src/routes/PageRoute.js
+++ front/src/routes/PageRoute.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import DownloadPage from "../pages/DownloadPage";
@@ -7,15 +7,15 @@ import LoginPage from "../pages/LoginPage";
 import QnaPage from "../pages/QnaPage";
 
 const PageRoute = () => {
-    return (
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/download" component={DownloadPage} />
-          <Route exact path="/help" component={HelpPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/qna" component={QnaPage} />
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/download" component={DownloadPage} />
+      <Route exact path="/help" component={HelpPage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/qna" component={QnaPage} />
+    </Switch>
+  );
+};
 
 export default PageRoute;
