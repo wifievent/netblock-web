5ac665b72bb4f0e9d2802535a54ff67ee8188509
Two-Weeks <webmon@kakao.com>
2021-11-07 19:46:29 +0900

    🔨 refactor: SVG camel

--- front/src/components/MenuSVG.js
+++ front/src/components/MenuSVG.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const MenuSVG = () => {
   return (
@@ -9,10 +9,10 @@ const MenuSVG = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="feather feather-menu"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-menu"
     >
       <line x1="3" y1="12" x2="21" y2="12"></line>
       <line x1="3" y1="6" x2="21" y2="6"></line>
