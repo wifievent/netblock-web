9922c9628561f1738e24dbc7a332b3ab408476f0
Two-Weeks <webmon@kakao.com>
2021-11-07 21:46:31 +0900

    🌱 Add: route

--- front/src/routes/PageRoute.js
+++ front/src/routes/PageRoute.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import MainPage from "../pages/MainPage";
-import DownloadPage from "../pages/DownloadPage";
-import HelpPage from "../pages/HelpPage";
-import LoginPage from "../pages/LoginPage";
-import QnaPage from "../pages/QnaPage";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import MainPage from '../pages/MainPage';
+import DownloadPage from '../pages/DownloadPage';
+import HelpPage from '../pages/HelpPage';
+import LoginPage from '../pages/LoginPage';
+import LogoutPage from '../pages/LogoutPage';
+import QnaPage from '../pages/QnaPage';
+import RegisterPage from '../pages/RegisterPage';
 
 const PageRoute = () => {
   return (
@@ -13,6 +15,8 @@ const PageRoute = () => {
       <Route exact path="/download" component={DownloadPage} />
       <Route exact path="/help" component={HelpPage} />
       <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/logout" component={LogoutPage} />
+      <Route exact path="/register" component={RegisterPage} />
       <Route exact path="/qna" component={QnaPage} />
     </Switch>
   );
