70de55d7e93e14e44e56e5259d4d3a1214732f65
Two-Weeks <webmon@kakao.com>
2021-11-08 16:37:56 +0900

    🌱 Add: qna page contents

--- front/src/pages/FaqPage.js
+++ front/src/pages/FaqPage.js
@@ -14,7 +14,9 @@ const textStyle = {
 
 const Faq = () => {
   const [open1, setOpen1] = useState(false);
-  //const [open2, setOpen2] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+  const [open4, setOpen4] = useState(false);
 
   return (
     <>
@@ -46,24 +48,73 @@ const Faq = () => {
           </Collapse>
         </div>
         <hr className="faq-hr" />
+        <div className="faq-q">
+          <button
+            style={btnStyle}
+            onClick={() => setOpen2(!open2)}
+            aria-controls="faq-text"
+            aria-expanded={open2}
+          >
+            Q. 프로그램을 항상 켜놓고 있어야 하나요?
+          </button>
+          <Collapse in={open2}>
+            <div id="faq-text" style={textStyle}>
+              차단시키고 싶은 디바이스의 정책을 걸어놓은 시간에
+              <br />
+              프로그램도 실행되고 있어야 차단이 가능합니다.
+            </div>
+          </Collapse>
+        </div>
+        <hr className="faq-hr" />
+        <div className="faq-q">
+          <button
+            style={btnStyle}
+            onClick={() => setOpen3(!open3)}
+            aria-controls="faq-text"
+            aria-expanded={open3}
+          >
+            Q. 프로그램을 다운받았는데 실행이 되지 않습니다
+          </button>
+          <Collapse in={open3}>
+            <div id="faq-text" style={textStyle}>
+              NetBlock은 윈도우 환경의 경우
+              <br />
+              다운로드 버튼 위에 있는 두개의 프로그램을 설치해야 실행이
+              가능합니다.
+            </div>
+          </Collapse>
+        </div>
+        <hr className="faq-hr" />
+        <div className="faq-q">
+          <button
+            style={btnStyle}
+            onClick={() => setOpen4(!open4)}
+            aria-controls="faq-text"
+            aria-expanded={open4}
+          >
+            Q. 프로그램을 실행할 경우 컴퓨터의 부하는 없나요?
+          </button>
+          <Collapse in={open4}>
+            <div id="faq-text" style={textStyle}>
+              NetBlock Inpath환경으로 디바이스를 차단시키기 때문에
+              <br />
+              서버의 부담이 적습니다.
+            </div>
+          </Collapse>
+        </div>
       </div>
     </>
   );
 };
 
-const Qna = () => {
-  return <div></div>;
-};
-
-const QnaPage = () => {
+const FaqPage = () => {
   return (
     <div>
       <Container>
         <Faq />
       </Container>
-      <Qna />
     </div>
   );
 };
 
-export default QnaPage;
+export default FaqPage;
