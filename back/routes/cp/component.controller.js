ffd5ba9a6fc3ed0b9a63fd2faf19ce8c06e473a7
hackloud <rladydrlaqkq@gmail.com>
2021-11-11 21:07:17 +0900

    update: component file isn't required

--- back/routes/cp/component.controller.js
+++ back/routes/cp/component.controller.js
@@ -6,23 +6,25 @@ const create = async (req, res, next) => {
   try {
     const userId = req.user.id;
     const { title, content } = req.body;
-    if (!title || !content || !req.file)
+    if (!title || !content)
       return res.status(400).json({ msg: 'invalid input' });
-
-    const src = req.file.originalname;
-    const size = req.file.size;
 
     await sequelize.transaction(async t => {
       const user = await User.findByPk(userId);
       const component = await Component.create({
         title, content,
       });
-      await File.create({
-        src, size,
-        sid: getHash(src + userId + Date.now(), user.salt),
-        userId,
-        componentId: component.id,
-      });
+
+      if (req.file) {
+        const src = req.file.originalname;
+        const size = req.file.size;
+        await File.create({
+          src, size,
+          sid: getHash(src + userId + Date.now(), user.salt),
+          userId,
+          componentId: component.id,
+        });
+      }
     });
 
     return res.status(201).json({ msg: 'component create success' });
