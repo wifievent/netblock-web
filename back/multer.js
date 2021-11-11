cff3123e0e6472f094bd17e99a1e96b33f5ddc9a
hackloud <rladydrlaqkq@gmail.com>
2021-11-11 19:26:58 +0900

    refactor: divide multer middleware

---
+++ back/multer.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+const path = require('path');
+const { getRandom, getHash } = require(path.resolve(__dirname, 'hashing'));
+
+const fileFilter = (req, file, cb) => {
+  const fileTypeCategory = file.mimetype.split('/')[0];
+  if (fileTypeCategory !== 'image')
+      cb({ message: "invalid file type (only image file)" }, false);
+  cb(null, true);
+}
+
+const storage = multer.diskStorage({
+      destination(req, file, cb) {
+          cb(null, 'uploads/');
+      },
+      filename(req, file, cb) {
+          const hashFileName = getHash(file.originalname + Date.now(), getRandom());
+          cb(null, hashFileName + path.extname(file.originalname));
+      },
+  });
+
+const imgUp = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: fileFilter,
+}).single('img');
+  
+module.exports = {
+  imgUp,
+}
