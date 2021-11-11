dffa9f2805ba36d8530621d097940f176ab09584
hackloud <rladydrlaqkq@gmail.com>
2021-11-11 20:56:54 +0900

    add: create component logic

--- back/routes/cp/component.controller.js
+++ back/routes/cp/component.controller.js
@@ -1,6 +1,34 @@
+const path = require('path');
+const { User, File, Component, sequelize } = require(path.resolve(__dirname, '..', '..', 'models'));
+const { getHash } = require(path.resolve(__dirname, '..', '..', 'hashing'));
+
 const create = async (req, res, next) => {
-  const { title, content } = req.body;
-  //req.file
+  try {
+    const userId = req.user.id;
+    const { title, content } = req.body;
+    if (!title || !content || !req.file)
+      return res.status(400).json({ msg: 'invalid input' });
+
+    const src = req.file.originalname;
+    const size = req.file.size;
+
+    await sequelize.transaction(async t => {
+      const user = await User.findByPk(userId);
+      const component = await Component.create({
+        title, content,
+      });
+      await File.create({
+        src, size,
+        sid: getHash(src + userId + Date.now(), user.salt),
+        userId,
+        componentId: component.id,
+      });
+    });
+
+    return res.status(201).json({ msg: 'component create success' });
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = {
