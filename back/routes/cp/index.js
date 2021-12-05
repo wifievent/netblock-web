5a6f1a014836f9d290edb0590f421b6c3b0fe1b9
hackloud <rladydrlaqkq@gmail.com>
2021-12-05 23:10:59 +0900

    refactor: router name (componet to page)

--- back/routes/cp/index.js
+++ back/routes/cp/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const controller = require('./cp.controller');
-const componentController = require('./component.controller');
+const pageController = require('./page.controller');
 const { isLoggedIn } = require(path.resolve(__dirname, '..', 'middlewares', 'middlewares'));
 const { imgUp } = require(path.resolve(__dirname, '..', '..', 'multer'));
 
 router.get('/', controller.download);
 
-router.post('/component', isLoggedIn, imgUp, componentController.create);
-router.patch('/component', isLoggedIn, imgUp, componentController.update);
-router.get('/component', isLoggedIn, componentController.read);
+router.post('/component', isLoggedIn, imgUp, pageController.create);
+router.patch('/component', isLoggedIn, imgUp, pageController.update);
+router.get('/component', isLoggedIn, pageController.read);
 
 module.exports = router;
