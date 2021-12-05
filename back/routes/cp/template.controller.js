241daec4ae90cd1d59e508f93381960fe94e8a09
hackloud <rladydrlaqkq@gmail.com>
2021-12-05 23:41:33 +0900

    add: page template router

---
+++ back/routes/cp/template.controller.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { Page, User } = require(path.resolve(__dirname, '..', '..', 'models'));
+const logger = require('../../config/winston');
+
+const read = async (req, res, next) => {
+
+}
+
+const render = async (req, res, next) => {
+
+}
+
+module.exports = {
+  read,
+  render
+}
