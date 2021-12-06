7850d07383c3b31d4f019cf105ccd6a6e283905a
hackloud <rladydrlaqkq@gmail.com>
2021-12-06 09:09:58 +0900

    update: page update, read logic
    single page logic -> multi page logic

--- back/routes/cp/page.controller.js
+++ back/routes/cp/page.controller.js
@@ -55,24 +55,25 @@ const create = async (req, res, next) => {
 
 const update = async (req, res, next) => {
   const userId = req.user.id;
+  const pageId = req.params.id;
   const { title, content } = req.body;
   if (!title || !content) {
     logger.error('invalid input');
     return res.status(400).json({ msg: 'invalid input' });
   }
   
-  const page = await Page.findOne({
-    where: { userId }
+  const page = await Page.count({
+    where: { id: pageId, userId }
   }).catch((err) => {
     logger.error(err);
     console.error(err);
     return next(err);
   });
-
   if (!page) {
     logger.error('invalid access');
     return res.status(403).json({ msg: 'invalid access' });
   }
+
   await sequelize.transaction(async t => {
     const user = await User.findByPk(userId).catch((err) => {
       logger.error(err);
@@ -133,13 +134,25 @@ const update = async (req, res, next) => {
 }
 
 const read = async (req, res, next) => {
+  const pageId = req.params.id;
   const userId = req.user.id;
-  const page = await Page.findOne({
-    where: userId,
-    include: {
-      model: File
-    }
-  });
+
+  let page;
+  if (Number.isNaN(pageId)) {
+    page = await Page.findAll({
+      where: { userId },
+      include: {
+        model: File
+      }
+    });
+  } else {
+    page = await Page.findOne({
+      where: { id: pageId, userId },
+      include: {
+        model: File
+      }
+    });
+  }
   logger.info('get component success');
   return res.status(200).json(page);
 }
