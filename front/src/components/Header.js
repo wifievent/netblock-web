1bc004c014eadbf1fc970cb0e5324d4e437be4ef
Two-Weeks <webmon@kakao.com>
2021-12-06 21:24:02 +0900

    🔨 fix : header

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -24,11 +24,9 @@ const Header = () => {
   const [session, setSession] = useState(false);
 
   useEffect(() => {
-    axios
-      .get('/api/user/session', {}, { withCredentials: true })
-      .then((res) => {
-        setSession(true);
-      });
+    axios.get('/user/session', {}, { withCredentials: true }).then((res) => {
+      setSession(true);
+    });
     console.log('useEffect');
     console.log(session);
   });
@@ -79,10 +77,12 @@ const Header = () => {
                     Captive Portal
                   </NavDropdown.Item>
                 </NavDropdown>
-                {session ? <Nav.Link href="/edit" className="navItem" style={link}>
-                  Edit
-                </Nav.Link> : null}
-                
+                {session ? (
+                  <Nav.Link href="/mypage" className="navItem" style={link}>
+                    MyPage
+                  </Nav.Link>
+                ) : null}
+
                 <Nav.Link href="/help" className="navItem" style={link}>
                   Help
                 </Nav.Link>
@@ -146,9 +146,11 @@ const Header = () => {
                   </NavDropdown.Item>
                 </NavDropdown>
 
-                <Nav.Link href="/edit" style={link}>
-                  Edit
-                </Nav.Link>
+                {session ? (
+                  <Nav.Link href="/mypage" style={link}>
+                    MyPage
+                  </Nav.Link>
+                ) : null}
 
                 <Nav.Link href="/help" style={link}>
                   Help
@@ -158,7 +160,7 @@ const Header = () => {
                   FAQ
                 </Nav.Link>
 
-                <Nav.Link href={session ? 'logout' : 'login'} style={link}>
+                <Nav.Link href={session ? '/logout' : '/login'} style={link}>
                   {session ? 'logout' : 'login'}
                 </Nav.Link>
               </Nav>
