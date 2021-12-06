9e0d034af7a2658caf02e992e86b0bb3afa9e6de
Two-Weeks <webmon@kakao.com>
2021-12-06 21:31:16 +0900

    🌱 Add: Terms of Privacy

--- front/src/pages/TermsPage.js
+++ front/src/pages/TermsPage.js
@@ -11,7 +11,7 @@ const container = {
 
 const divTitle = {
   textAlign: 'center',
-  margin: '1rem',
+  margin: '2rem 0 0 0',
   fontSize: '25px',
   fontWeight: '700',
 };
@@ -45,15 +45,26 @@ const button = {
   fontSize: '1rem',
   fontWeight: 'bold',
   padding: '0.25rem 1rem',
+  marginBottom: '1rem',
   color: 'white',
   outline: 'none',
 };
 
 const TermsPage = () => {
-  const [checked, setChecked] = useState(false);
-  const handleChecked = () => {
-    setChecked(!checked);
+  const [checkedInputs, setCheckedInputs] = useState([]);
+
+  const changeHandler = (checked, id) => {
+    if (checked) {
+      setCheckedInputs([...checkedInputs, id]);
+      console.log('체크 반영 완료');
+    } else {
+      setCheckedInputs(checkedInputs.filter((el) => el !== id));
+      console.log('체크 해제 반영 완료');
+    }
   };
+
+  const isAllChecked = checkedInputs.length === 2;
+  const disabled = !isAllChecked;
 
   return (
     <Container style={container}>
@@ -253,19 +264,109 @@ const TermsPage = () => {
       <div style={div}>
         WiFiEvent 이용약관 동의 (필수)
         <input
-          style={input}
           type="checkbox"
-          id="termsChk"
-          value="termsOk"
-          name="termsChk"
-          checked={checked}
-          onChange={handleChecked}
-        />
+          style={input}
+          id="check1"
+          name="check2"
+          onChange={(e) => {
+            changeHandler(e.currentTarget.checked, 'check');
+          }}
+          checked={checkedInputs.includes('check') ? true : false}
+        ></input>
+        <label id="check" htmlFor="check"></label>
+      </div>
+      <div style={divText}>
+        개인정보보호법에 따라 WiFiEvent에 회원가입 신청하시는 분께 수집하는
+        개인정보의 항목, 개인정보의 수집 및 이용목적, 개인정보의 보유 및
+        이용기간, 동의 거부권 및 동의 거부 시 불이익에 관한 사항을 안내 드리오니
+        자세히 읽은 후 동의하여 주시기 바랍니다.
+        <br />
+        <br />
+        1. 수집하는 개인정보
+        <br />
+        이용자가 회원제 서비스를 이용하기 위해 회원가입을 할 경우, WiFiEvent는
+        서비스 이용을 위해 필요한 최소한의 개인정보를 수집합니다.
+        <br />
+        회원가입 시점에 WiFiEvent가 이용자로부터 수집하는 개인정보는 아래와
+        같습니다.
+        <br />
+        - 회원 가입 시에 ‘아이디, 비밀번호, 이름, e-mail’를 필수항목으로
+        수집합니다.
+        <br />
+        서비스 이용 과정에서 IP 주소, 쿠키, 서비스 이용 기록, 기기정보,
+        위치정보가 생성되어 수집될 수 있습니다.
+        <br />
+        구체적으로 1) 서비스 이용 과정에서 이용자에 관한 정보를 자동화된
+        방법으로 생성하여 이를 저장(수집)하거나,
+        <br />
+        2) 이용자 기기의 고유한 정보를 원래의 값을 확인하지 못 하도록 안전하게
+        변환하여 수집합니다. 서비스 이용 과정에서 위치정보가 수집될 수 있으며,
+        <br />
+        이와 같이 수집된 정보는 개인정보와의 연계 여부 등에 따라 개인정보에
+        해당할 수 있고, 개인정보에 해당하지 않을 수도 있습니다.
+        <br />
+        <br />
+        2. 수집한 개인정보의 이용
+        <br />
+        WiFiEvent 서비스(웹/앱 포함)의 회원관리, 서비스 개발・제공 및 향상,
+        안전한 인터넷 이용환경 구축 등 아래의 목적으로만 개인정보를 이용합니다.
+        <br />
+        - 회원 가입 의사의 확인, 이용자의 본인 확인, 이용자 식별, 회원탈퇴
+        의사의 확인 등 회원관리를 위하여 개인정보를 이용합니다.
+        <br />- 콘텐츠 등 기존 서비스 제공(광고 포함)에 더하여, 인구통계학적
+        분석, 서비스 방문 및 이용기록의 분석, 신규 서비스 요소의 발굴 및 기존
+        서비스 개선, 서비스를 악용하는 행위 탐지 등을 위하여 개인정보를
+        이용합니다.
+        <br />- 법령 및 WiFiEvent 이용약관을 위반하는 회원에 대한 이용 제한
+        조치, 부정 이용 행위를 포함하여 서비스의 원활한 운영에 지장을 주는
+        행위에 대한 방지 및 제재, 계정도용 및 부정거래 방지, 약관 개정 등의
+        고지사항 전달, 분쟁조정을 위한 기록 보존, 민원처리 등 이용자 보호 및
+        서비스 운영을 위하여 개인정보를 이용합니다.
+        <br />- 유료 서비스 제공에 따르는 본인인증, 구매 및 요금 결제, 상품 및
+        서비스의 배송을 위하여 개인정보를 이용합니다.
+        <br />- 광고성 정보 제공 등 마케팅 및 프로모션 목적으로 개인정보를
+        이용합니다.
+        <br />- 서비스 이용기록과 접속 빈도 분석, 서비스 이용에 대한 통계,
+        서비스 분석 및 통계에 따른 맞춤 서비스 제공 및 광고 게재 등에 개인정보를
+        이용합니다.
+        <br />- 보안, 프라이버시, 안전 측면에서 이용자가 안심하고 이용할 수 있는
+        서비스 이용환경 구축을 위해 개인정보를 이용합니다.
+        <br />
+        <br />
+        3. 개인정보의 보관기간
+        <br />
+        회사는 원칙적으로 이용자의 개인정보를 회원 탈퇴 시 지체없이 파기하고
+        있습니다.
+        <br />
+        단, 이용자에게 개인정보 보관기간에 대해 별도의 동의를 얻은 경우, 또는
+        법령에서 일정 기간 정보보관 의무를 부과하는 경우에는 해당 기간 동안
+        개인정보를 안전하게 보관합니다.
+        <br />
+        <br />
+        4. 개인정보 수집 및 이용 동의를 거부할 권리
+        <br />
+        이용자는 개인정보의 수집 및 이용 동의를 거부할 권리가 있습니다. 회원가입
+        시 수집하는 최소한의 개인정보, 즉, 필수 항목에 대한 수집 및 이용 동의를
+        거부하실 경우, 회원가입이 어려울 수 있습니다.
+      </div>
+      <div style={div}>
+        개인정보 수집 및 이용 동의 (필수)
+        <input
+          type="checkbox"
+          style={input}
+          id="check2"
+          name="check2"
+          onChange={(e) => {
+            changeHandler(e.currentTarget.checked, 'check2');
+          }}
+          checked={checkedInputs.includes('check2') ? true : false}
+        ></input>
+        <label id="check2" htmlFor="check2"></label>
       </div>
       <button
         className="termsBtn"
         style={button}
-        disabled={!checked}
+        disabled={disabled}
         onClick={() => {
           window.location.href = '/register';
         }}
